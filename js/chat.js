7bbe70eac66d836a2aedc2e29f6b527160d98719
yash <yhdelectric@gmail.com>
2018-07-18 21:08:00 +0530

    edited chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,19 +1,36 @@
-input[type=text] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: none;
-    background-color: #2f3136;
-    color: white;
+ var config = {
+    apiKey: "AIzaSyDw5YEFrcJZfpHg-LOJvEb66QzlCiHLb6o",
+    authDomain: "chat-app-dc9d8.firebaseapp.com",
+    databaseURL: "https://chat-app-dc9d8.firebaseio.com",
+    storageBucket: "chat-app-dc9d8.appspot.com",
+};
+firebase.initializeApp(config);
+
+var chatData = firebase.database().ref();
+
+function pushMessage(event) {
+  if (event.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    chatData.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
 }
 
-div.chat {
-    color: white;
-    background-color: #2f3136;
-    width: 100%;
-    border: none;
-    box-sizing: border-box;
-    padding: 12px 20px;
-    margin: 8px 0;
+$('#messageInput').keypress(pushMessage);
+
+chatData.on("child_added", showMessage);
+
+function showMessage(msg) {
+  var message = msg.val();
+  var messageSender = message.name;
+  var messageContent = message.text;
+
+  var messageEl = $("<div/>").addClass("message");
+  var senderEl = $("<span/>").text(messageSender + ": ");
+  var contentEl = $("<span/>").text(messageContent);
+  
+  messageEl.append(senderEl);
+  messageEl.append(contentEl);
+  $('#messages').append(messageEl);
 }
